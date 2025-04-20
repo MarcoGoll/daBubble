b13358da1a5532a5feeed6f1c14d15ecfae5e301
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-04-20 15:15:35 +0200

    create: component footer-simple

--- src/app/_shared/components/footer-simple/footer-simple.component.ts
+++ src/app/_shared/components/footer-simple/footer-simple.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer-simple',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer-simple.component.html',
-  styleUrl: './footer-simple.component.scss'
+  styleUrl: './footer-simple.component.scss',
 })
-export class FooterSimpleComponent {
-
-}
+export class FooterSimpleComponent {}
