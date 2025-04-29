2cc57b6ecb66900a16e66790859454b0c7b12ac9
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-04-29 22:31:11 +0200

    create: headline-container + global styles for icon-button

--- src/app/homepage/conversation-list/conversation-list.component.ts
+++ src/app/homepage/conversation-list/conversation-list.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { UserService } from '../../_shared/services/firebase/user.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-conversation-list',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatIconModule, MatTooltipModule, MatButtonModule],
   templateUrl: './conversation-list.component.html',
   styleUrl: './conversation-list.component.scss',
 })
-export class ConversationListComponent {}
+export class ConversationListComponent {
+  userService = inject(UserService);
+}
