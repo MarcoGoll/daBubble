7cfa7c08b1bea34166801d4d51071b26e46dd814
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-04-30 06:52:45 +0200

    create: mat-akordion for conversation-list-component

--- src/app/homepage/conversation-list/conversation-list.component.ts
+++ src/app/homepage/conversation-list/conversation-list.component.ts
@@ -1,17 +1,33 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  inject,
+  ChangeDetectionStrategy,
+  signal,
+} from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { UserService } from '../../_shared/services/firebase/user.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-conversation-list',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, MatTooltipModule, MatButtonModule],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatExpansionModule,
+    CommonModule,
+  ],
   templateUrl: './conversation-list.component.html',
   styleUrl: './conversation-list.component.scss',
 })
 export class ConversationListComponent {
   userService = inject(UserService);
+  readonly panelOpenStateChannels = signal(false);
+  readonly panelOpenStateUser = signal(false);
 }
