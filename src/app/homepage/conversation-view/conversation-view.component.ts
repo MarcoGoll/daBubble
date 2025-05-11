ca227d01d12bfdd16ca7a136fe29ddabbe75c45c
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-05-11 17:34:10 +0200

    create: basic logic to create a new message within conversation

--- src/app/homepage/conversation-view/conversation-view.component.ts
+++ src/app/homepage/conversation-view/conversation-view.component.ts
@@ -1,14 +1,35 @@
 import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ConversationService } from '../../_shared/services/firebase/conversation.service';
+import { FormsModule } from '@angular/forms';
+import { AuthenticationService } from '../../_shared/services/firebase/authentication.service';
+import { Message } from '../../_shared/interfaces/message';
 
 @Component({
   selector: 'app-conversation-view',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, FormsModule],
   templateUrl: './conversation-view.component.html',
   styleUrl: './conversation-view.component.scss',
 })
 export class ConversationViewComponent {
   conversationService = inject(ConversationService);
+  authService = inject(AuthenticationService);
+  newMessageText: string = '';
+
+  async createNewMessage() {
+    if (this.authService.currentLoggedInUser?.displayName) {
+      let newMessage: Message = {
+        timestamp: Date.now(),
+        sender: this.authService.currentLoggedInUser.displayName,
+        text: this.newMessageText,
+      };
+      this.conversationService
+        .getCurrentConversation()
+        .messageBlock.messages.push(newMessage);
+      await this.conversationService.updateConversation(
+        this.conversationService.getCurrentConversation()
+      );
+    }
+  }
 }
