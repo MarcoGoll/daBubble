15e852d4d7c0823dd72e6761c80c2f86e7e3b069
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2025-05-23 06:27:30 +0200

    create: global toggleisThreadDetailListOpen

--- src/app/_shared/services/global-adjustments.service.ts
+++ src/app/_shared/services/global-adjustments.service.ts
@@ -5,10 +5,15 @@ import { Injectable } from '@angular/core';
 })
 export class GlobalAdjustmentsService {
   isConversationListOpen: boolean = true;
+  isThreadDetailListOpen: boolean = true;
 
   constructor() {}
 
   toggleIsConversationListOpen() {
     this.isConversationListOpen = !this.isConversationListOpen;
+  }
+
+  toggleisThreadDetailListOpen() {
+    this.isThreadDetailListOpen = !this.isThreadDetailListOpen;
   }
 }
